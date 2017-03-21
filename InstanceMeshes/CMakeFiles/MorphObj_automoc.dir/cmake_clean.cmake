2a0c079c17331ea4915dc50ed0e06141815c53c1
jmacey <jmacey@bournemouth.ac.uk>
2017-03-21 12:57:56 +0000

    added a new instance demo for meshes

--- InstanceMeshes/CMakeFiles/MorphObj_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "MorphObj_automoc.cpp"
-  "CMakeFiles/MorphObj_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/MorphObj_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
